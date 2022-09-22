2b7de102b6fbdbb9045e8e1586f9bbb050d06fcd
Daniel Oliveira <apenasdanieloliveira@gmail.com>
2022-09-22 11:49:44 -0300

    Equals, hashcode, outros Sets e iterator

--- src/br/com/alura/TestaCursoComAluno.java
+++ src/br/com/alura/TestaCursoComAluno.java
@@ -1,5 +1,8 @@
 package br.com.alura;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class TestaCursoComAluno {
 
 	public static void main(String[] args) {
@@ -18,10 +21,28 @@ public class TestaCursoComAluno {
 		javaColecoes.matricula(a3);
 		
 		System.out.println("Todos os alunos matriculados: ");
+		
+//		Set<Aluno> alunos = javaColecoes.getAlunos();
+//		Iterator<Aluno> iterador = alunos.iterator();
+//		while(iterador.hasNext()) {
+//			Aluno proximo = iterador.next();
+//		System.out.println(proximo);
+//		}
+		
 		javaColecoes.getAlunos().forEach(a -> {
 			System.out.println(a);
 		});
 		
+		System.out.println("O aluno " + a1 + "está matriculado?");
+		System.out.println(javaColecoes.estaMatriculado(a1));
+		
+		
+		Aluno turini = new Aluno("Rodrigo Turini", 34672);
+		System.out.println("E esse Turini, esta matriculado?");
+		System.out.println(javaColecoes.estaMatriculado(turini));
+		
+		System.out.println("O a1 e equals ao Turini?");
+		System.out.println(a1.equals(turini));
 	}
 
 }
